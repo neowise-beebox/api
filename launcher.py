2f9f59735ef3d4b4b9bb00fb20d9f70521790487
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-04-29 21:11:40 -0300

    api route to create new bee occurrence

--- launcher.py
+++ launcher.py
@@ -1,12 +1,27 @@
 #!/usr/local/bin python
 
 from flask import Flask
+from flask import jsonify
+from flask import request
+from database.dao import beedao
 
 app = Flask( __name__ )
 
-@app.route("/")
+# Welcome to BeeBox
+@app.route("/", methods=["GET"])
 def welcome():
     return "Welcome to BeeBox!"
+
+# Creates new bee occurrence
+@app.route("/savebee", methods=["POST"])
+def savebee():
+    beedao.save(request.form["beedata"])
+    return jsonify( "ok" )
+
+# Lists all bee occurrences
+@app.route("/listbee", methods=["GET"])
+def listbees():
+    return jsonify( beedao.listBeeOccurrences() )
 
 if __name__ == '__main__':
     app.run()
