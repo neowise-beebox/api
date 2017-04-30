70b293d87c76d1671fa0d0fda75c884acac25324
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-04-30 03:14:05 -0300

    saving bee report and local weather

--- database/dao/beedao.py
+++ database/dao/beedao.py
@@ -2,13 +2,15 @@
 #-*- coding: utf-8 -*-
 
 import json
+from weatherhelper import weatherhelper
 from database import megachilidaeconnection
 
 connection = megachilidaeconnection.getConnection()
 
 ## Implementar o id e o device_id
 def newBee(beedata):
-    return {
+    # print weatherhelper.getWeatherUsingCoord(beedata["latitude"], beedata["longitude"])
+    data = {
         "description": beedata["description"],
         "image_path": beedata["picture"],
         "species": beedata["species"],
@@ -17,11 +19,11 @@ def newBee(beedata):
             "lng": beedata["longitude"]
         }
     }
+    data.update(weatherhelper.getWeatherUsingCoord(beedata["latitude"], beedata["longitude"]))
+    return data
 
 def save(beejson):
-    print "sended data ==> {}".format( beejson )
-    print "sended data type ==> {}".format(type( beejson ))
-    connection.post( "/bees", newBee( json.loads(beejson) ) )
+    connection.post("/bees", newBee( json.loads(beejson) ) )
 
 def listBeeOccurrences():
     bee_list = []
---
+++ weatherhelper/weatherhelper.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import json
+import requests
+
+apidae_api_key = "0079f159f4ced7bbe0694468132deee8"
+
+def getWeatherUsingCoord(lat, lng):
+    weather_api_url = "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}".format(lat, lng, apidae_api_key)
+    weather_info = requests.get( weather_api_url ).content
+    return mount_dict(weather_info)
+
+def mount_dict(info):
+    info = json.loads(info)
+    return {
+        "state": info["name"] if info.has_key("name") else "",
+        "weather": {
+            "temp": info["main"]["temp"] if info["main"].has_key("temp") else "",
+            "pressure": info["main"]["pressure"] if info["main"].has_key("pressure") else "",
+            "humidity": info["main"]["humidity"] if info["main"].has_key("humidity") else "",
+            "max": info["main"]["temp_max"] if info["main"].has_key("temp_max") else "",
+            "min": info["main"]["temp_min"] if info["main"].has_key("temp_min") else "",
+        },
+        "wind": {
+            "speed": info["wind"]["speed"] if info["wind"].has_key("speed") else "",
+            "deg": info["wind"]["deg"] if info["wind"].has_key("deg") else "",
+        }
+    }
